c8c84d79d857e58dc482d5c868a6e98359be51d5
Adryan Oliveira <blessedt402@gmail.com>
2026-02-04 15:57:44 -0300

    Initial commit

--- contexts/SalonDataContext.tsx
+++ contexts/SalonDataContext.tsx
@@ -400,6 +400,7 @@ export function SalonDataProvider({ children }: { children: React.ReactNode }) {
 
   const value: SalonDataContextType = {
     ...data,
+    realizedRevenueByDate: data.realizedRevenueByDate || {},
     addClient,
     updateClient,
     deleteClient,
